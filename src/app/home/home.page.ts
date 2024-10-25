8b1cdb44cea0c3c19a0aeec566f82429e61451b6
Zoechbauer <hans.zoechbauer@aon.at>
2024-10-25 19:14:50 +0200

    qr code functions and libs added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  myAngularxQrCode: string = '';
+  qrCodeDownloadLink: SafeUrl = '';
 
-  constructor() {}
+  constructor() {
+    this.myAngularxQrCode = 'Your initial QR code data';
+  }
 
+  // TODO change type any
+  generateQRCode(data: any) {
+    this.myAngularxQrCode = data;
+  }
+
+  onChangeURL(url: SafeUrl) {
+    this.qrCodeDownloadLink = url;
+  }
+
+  printQRCode() {
+    const printContent = document.getElementById('qrcode');
+    const windowPrint = window.open('', '', 'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0');
+    if (windowPrint && printContent) {
+      windowPrint.document.write(printContent.innerHTML);
+      windowPrint.document.close();
+      windowPrint.focus();
+      windowPrint.print();
+      windowPrint.close();
+    }
+  }
 }
